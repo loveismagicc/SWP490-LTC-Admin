d90379d83e7a8fc88ebc88cbb7ebae87d2e67697
harusora <longcnhe141293@gmail.com>
2025-07-02 16:37:21 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 8080, // hoặc port bạn đang dùng
+    host: true, // để bind vào 0.0.0.0
+    allowedHosts: ['swp490-ltc-admin.onrender.com'],
+  }
 })
